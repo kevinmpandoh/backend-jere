5cd26f42c328f7c3d27730b5043b033ec07526ab
Kevin Mclaren Pandoh <kevinmpandoh@gmail.com>
2025-09-19 22:22:08 +0800

    fix error

--- src/config/mailer.ts
+++ src/config/mailer.ts
@@ -2,22 +2,22 @@ import nodemailer from "nodemailer";
 import { env } from "./env";
 
 export const transporter = nodemailer.createTransport({
-  host: env.SMTP_HOST,
-  port: env.SMTP_PORT,
-  secure: env.SMTP_SECURE === "true",
+  host: env.SMTP_HOST!,
+  port: env.SMTP_PORT!,
+  secure: env.SMTP_SECURE! === "true",
   auth: {
-    user: env.SMTP_USER,
-    pass: env.SMTP_PASSWORD,
+    user: env.SMTP_USER!,
+    pass: env.SMTP_PASSWORD!,
   },
 });
 
 export async function sendMail(to: string, subject: string, html: string) {
   console.log(
-    env.SMTP_HOST,
-    env.SMTP_PORT,
+    env.SMTP_HOST!,
+    env.SMTP_PORT!,
     env.SMTP_SECURE === "true",
-    env.SMTP_USER,
-    env.SMTP_PASSWORD,
+    env.SMTP_USER!,
+    env.SMTP_PASSWORD!,
     "TESTING JO"
   );
   const info = await transporter.sendMail({
